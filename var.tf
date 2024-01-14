e8dd23db9d83aecbf891c1aab26e5f81aaa671f6
X103703 <desboisdimitri@yahoo.fr>
2024-01-14 10:34:16 +0100

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,26 @@
+variable "cg_acces_key" {
+  type = string
+  default = "AKIA3Y2AWHAVBZFU64SH"
+
+}
+
+variable "cg_secret_key" {
+  type = string
+  default = "aiU/J+E+q2FEiE8j7cG9Zcz9mDg185FXioJYjxmv"
+
+}
+
+variable "cg_account" {
+  type = string
+  default = "809200138282"
+}
+
+variable "cg_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "cg_subnet_ids" {
+  type = string
+  default = ""
+}
